50d24562c7efbb0de84c29e435e7265d542604ac
elvinpeng <elvinpeng@tencent.com>
2020-11-25 15:40:49 +0800

    feat: 实现 deleteMemo 和 editMemo

--- src/cloudfunctions/memo-store/logic/index.ts
+++ src/cloudfunctions/memo-store/logic/index.ts
@@ -1,4 +1,4 @@
-import { getUserInfo, getCollection, CollectionName } from '../common/tcb'
+import { getUserInfo, getCollection, CollectionName } from '../common/tcb';
 import { Memo } from '../types';
 
 const memoCollection = getCollection(CollectionName.MemoStore);
@@ -6,15 +6,19 @@ const memoCollection = getCollection(CollectionName.MemoStore);
 async function addMemo(memo: Memo) {
   const { customUserId } = await getUserInfo();
   console.log('准备添加数据', memo);
-  const { id } = await memoCollection.add({
-    customUserId,
-    ...memo
-  }).catch((e) => {
-    console.error('添加失败', e);
-  }) || {};
+  const { code, message, id } =
+    (await memoCollection
+      .add({
+        ...memo,
+        customUserId,
+      })
+      .catch((e: Error) => {
+        console.error('添加失败', e);
+      })) || {};
 
-  if (!id) {
-    return;
+  if (!id || code) {
+    console.error(`添加失败，${code} : ${message}`);
+    return null;
   }
 
   console.log('添加成功', id);
@@ -22,16 +26,47 @@ async function addMemo(memo: Memo) {
 }
 
 async function deleteMemo(memo: Memo) {
-  
+  const { customUserId } = await getUserInfo();
+  const { code, message } =
+    (await memoCollection
+      .where({
+        customUserId, // 通过 customUserId 保证只有用户本人可以删除
+        id: memo.id,
+      })
+      .update({
+        isDeleted: 1,
+      })
+      .catch((e: Error) => {
+        console.error('删除失败', e);
+      })) || {};
+
+  if (code) {
+    console.error(`删除失败，${code} : ${message}`);
+    return null;
+  }
+
+  return { id: memo.id };
 }
 
 async function editMemo(memo: Memo) {
-  
+  const { customUserId } = await getUserInfo();
+  const { code, message } =
+    (await memoCollection
+      .where({
+        customUserId, // 通过 customUserId 保证只有用户本人可以删除
+        id: memo.id,
+      })
+      .update(memo)
+      .catch((e: Error) => {
+        console.error('更新失败', e);
+      })) || {};
+
+  if (code) {
+    console.error(`更新失败，${code} : ${message}`);
+    return null;
+  }
+
+  return { id: memo.id };
 }
 
-export {
-  addMemo,
-  deleteMemo,
-  editMemo,
-}
-
+export { queryMemo, addMemo, deleteMemo, editMemo };
