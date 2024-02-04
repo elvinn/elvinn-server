faef53816640e9299f7a515e6535c91accc59ac0
elvinpeng <elvinpeng@tencent.com>
2024-02-04 15:21:24 +0800

    feat: 完成基本样式

--- src/app/tic-tac-toe/page.tsx
+++ src/app/tic-tac-toe/page.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { MouseEventHandler, useState } from "react";
+import { MouseEventHandler, ReactElement, useState } from "react";
 import { isWin } from './util';
+import './page.css';
 
 /**
  * 棋格组件
@@ -11,13 +12,20 @@ function Square({ content, rowIndex, colIndex }: {
   rowIndex: number; // 行坐标
   colIndex: number; // 列坐标
 }) {
+  let contentEl: ReactElement | null = null;
+  if (content === 'X') {
+    contentEl = <text className="text-red-600">X</text>
+  } else if (content === 'O') {
+    contentEl = <text className="text-green-600">O</text>
+  }
+
   return (
     <div
       className="aspect-square w-24 bg-white text-black flex items-center justify-center text-3xl font-bold"
       data-row={rowIndex}
       data-col={colIndex}
     >
-      {content}
+      {contentEl}
     </div>
   )
 }
@@ -87,25 +95,30 @@ function Board({ xIsNext, squares, enabled, onPlay }: {
 }
 
 /**
- * 下棋历史组建
+ * 落子记录组件
  */
 function MoveHistory({ moves, squares }: { moves: { rowIndex: number, colIndex: number }[]; squares: string[][] }) {
   const moveEls = moves.map((move, index) => {
     const { rowIndex, colIndex } = move;
     return (
       <li key={`${rowIndex}-${colIndex}`}>
-        {index + 1}. {squares[rowIndex][colIndex]} 落子于第 {rowIndex + 1} 行，第 {colIndex + 1} 列
+        {index + 1}. {squares[rowIndex][colIndex]} 落于第 {rowIndex + 1} 行，第 {colIndex + 1} 列
       </li>
     )
   });
 
-  return <ul>{moveEls}</ul>
+  return (
+    <ul className="bg-white p-4 rounded-lg w-80 h-80 overflow-auto">
+      <h2 className="text-2xl font-bold text-gray-900 mb-4">落子记录</h2>
+      {moveEls}
+    </ul>
+  );
 }
 
 export default function TicTacToe() {
   const rowNum = 3;
   const colNum = 3;
-  const winnerLength = 2;
+  const winnerLength = 3;
 
   // 棋盘历史
   const [history, setHistory] = useState([Array(rowNum).fill(Array(colNum).fill(null))]);
@@ -115,11 +128,14 @@ export default function TicTacToe() {
   const [isEnd, setIsEnd] = useState(false);
   // 获胜者
   const [winner, setWinner] = useState(null as string | null);
+  // 是否展示落子记录
+  const [isMoveHistoryVisible, setMoveHistoryVisible] = useState(false);
 
   const currentStep = moves.length;
   const xIsNext = !!(currentStep % 2);
   const currentSquares = history[currentStep];
 
+  // 处理落子逻辑
   const handlePlay = ({
     nextSquares,
     rowIndex,
@@ -141,11 +157,37 @@ export default function TicTacToe() {
     }
   };
 
+  // 切换落子记录展示
+  const toggleMoveHistory = () => {
+    setMoveHistoryVisible(!isMoveHistoryVisible);
+  };
+
+  // 重置棋局，再来一盘
+  const handleNewGame = () => {
+    setHistory([Array(rowNum).fill(Array(colNum).fill(null))]);
+    setMoves([]);
+    setIsEnd(false);
+    setWinner(null);
+    setMoveHistoryVisible(false);
+  };
+
   return (
-    <main className="flex">
-      {winner && <div>{winner} 获胜</div>}
-      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} enabled={!isEnd} />
-      <MoveHistory squares={currentSquares} moves={moves} />
+    <main className="min-w-96 w-screen h-screen flex flex-col items-center min-h-screen bg-gray-100">
+      <h1 className="mt-24 mb-8 text-4xl font-bold text-gray-900">井字棋</h1>
+      <div className="w-full flex flex-wrap items-center justify-center">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} enabled={!isEnd} />
+        {isMoveHistoryVisible && <MoveHistory squares={currentSquares} moves={moves} />}
+      </div>
+      {winner && (
+        <>
+          <div className="mt-8 text-2xl font-bold text-gray-900">游戏结束！{winner} 获胜！</div>
+          <button onClick={handleNewGame} className="mt-8 bg-green-500 hover:bg-green-700 text-white font-bold py-2 w-48 rounded">再来一盘</button>
+          <button onClick={toggleMoveHistory} className="mt-2 bg-gray-300 hover:bg-gray-400 text-white font-bold py-2 w-48 rounded">
+            {isMoveHistoryVisible ? '隐藏' : '查看'}落子记录
+          </button>
+        </>
+      )}
+
     </main>
   );
 }
